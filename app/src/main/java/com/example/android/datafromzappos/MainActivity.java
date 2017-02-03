cfd1088ff80a174a4290e1601de7fec76b96f59c
xyuae <xyuae@connect.ust.hk>
2017-02-03 00:15:20 -0500

    first commit

---
+++ app/src/main/java/com/example/android/datafromzappos/MainActivity.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.datafromzappos;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.android.datafromzappos.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mSearchBoxEditText;
+
+    private TextView mUrlDisplayTextView;
+
+    private TextView mSearchResultsTextView;
+
+    // TODO (12) Create a variable to store a reference to the error message TextView
+    private TextView mErrorMessageTextView;
+
+    // TODO (24) Create a ProgressBar variable to store a reference to the ProgressBar
+    private ProgressBar mLoadingIndicatorProgressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSearchBoxEditText = (EditText) findViewById(R.id.et_search_box);
+
+        mUrlDisplayTextView = (TextView) findViewById(R.id.tv_url_display);
+        mSearchResultsTextView = (TextView) findViewById(R.id.tv_search_results_json);
+
+        // TODO (13) Get a reference to the error TextView using findViewById
+        mErrorMessageTextView = (TextView) findViewById(R.id.tv_error_message_display);
+        // TODO (25) Get a reference to the ProgressBar using findViewById
+        mLoadingIndicatorProgressBar = (ProgressBar) findViewById(R.id.pb_loading_indicator);
+    }
+
+    /**
+     * This method retrieves the search text from the EditText, constructs the
+     * URL (using {@link NetworkUtils}) for the Zappos item you'd like to find, displays
+     * that URL in a TextView, and finally fires off an AsyncTask to perform the GET request using
+     * our {@link QueryTask}
+     */
+    private void makeZapposSearchQuery() {
+        String Query = mSearchBoxEditText.getText().toString();
+        URL SearchUrl = NetworkUtils.buildUrl(Query);
+        mUrlDisplayTextView.setText(SearchUrl.toString());
+        new QueryTask().execute(SearchUrl);
+    }
+
+    // TODO (14) Create a method called showJsonDataView to show the data and hide the error
+    private void showJsonDataView() {
+        // Make sure the error is invisible
+        mErrorMessageTextView.setVisibility(View.INVISIBLE);
+        // Make sure the JSON data is visible
+        mSearchResultsTextView.setVisibility(View.VISIBLE);
+    }
+    private void showErrorMessage() {
+        // Make sure the error is visible
+        mErrorMessageTextView.setVisibility(View.VISIBLE);
+        // The JSON data is invisible
+        mSearchResultsTextView.setVisibility(View.INVISIBLE);
+    }
+    // TODO (15) Create a method called showErrorMessage to show the error and hide the data
+
+    public class QueryTask extends AsyncTask<URL, Void, String> {
+
+        // TODO (26) Override onPreExecute to set the loading indicator to visible
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            mLoadingIndicatorProgressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected String doInBackground(URL... params) {
+            URL searchUrl = params[0];
+            String SearchResults = null;
+            try {
+                SearchResults = NetworkUtils.getResponseFromHttpUrl(searchUrl);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return SearchResults;
+        }
+
+        @Override
+        protected void onPostExecute(String zapposSearchResults) {
+            // TODO (27) As soon as the loading is complete, hide the loading indicator
+            mLoadingIndicatorProgressBar.setVisibility(View.INVISIBLE);
+            if (zapposSearchResults != null && !zapposSearchResults.equals("")) {
+                // TODO (17) Call showJsonDataView if we have valid, non-null results
+                showJsonDataView();
+                mSearchResultsTextView.setText(zapposSearchResults);
+            } else {
+                showErrorMessage();
+            }
+            // TODO (16) Call showErrorMessage if the result is null in onPostExecute
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.action_search) {
+            makeZapposSearchQuery();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/android/datafromzappos/utilities/NetworkUtils.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.datafromzappos.utilities;
+
+import android.net.Uri;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * These utilities will be used to communicate with the network.
+ */
+
+/**
+ * These utilities will be used to communicate with the zappos servers.
+ */
+public final class NetworkUtils {
+    private final static String ZAPPOS_BASE_URL =
+            "https://api.zappos.com/Search";
+    final static String QUERY_PARAM = "term";
+    final static String KEY = "b743e26728e16b81da139182bb2094357c31d331";
+    final static String KEY_PARAM = "key";
+
+    /**
+     * Builds the URL used to talk to the weather server using a location. This location is based
+     * on the query capabilities of the weather provider that we are using.
+     *
+     * @param zapposSearchQuery The item that will be queried for.
+     * @return The URL to use to query the weather server.
+     */
+    public static URL buildUrl(String zapposSearchQuery) {
+        // COMPLETED (1) Fix this method to return the URL used to query Open Zappos's API
+        Uri builtUri = Uri.parse(ZAPPOS_BASE_URL).buildUpon()
+                .appendQueryParameter(QUERY_PARAM, zapposSearchQuery)
+                .appendQueryParameter(KEY_PARAM, KEY).build();
+        URL url = null;
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return url;
+    }
+
+    /**
+     * This method returns the entire result from the HTTP response.
+     *
+     * @param url The URL to fetch the HTTP response from.
+     * @return The contents of the HTTP response.
+     * @throws IOException Related to network and stream reading
+     */
+    public static String getResponseFromHttpUrl(URL url) throws IOException {
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+
+            Scanner scanner = new Scanner(in);
+            scanner.useDelimiter("\\A");
+
+            boolean hasInput = scanner.hasNext();
+            if (hasInput) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } finally {
+            urlConnection.disconnect();
+        }
+    }
+}
