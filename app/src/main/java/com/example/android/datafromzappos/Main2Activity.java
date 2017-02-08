6999cb23f4c24036ac9eac5291e85d1b8725df27
xyuae <xyuae@connect.ust.hk>
2017-02-08 12:34:56 -0500

    Animaiton online

--- app/src/main/java/com/example/android/datafromzappos/Main2Activity.java
+++ app/src/main/java/com/example/android/datafromzappos/Main2Activity.java
@@ -3,11 +3,14 @@ package com.example.android.datafromzappos;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.graphics.drawable.Drawable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 import com.example.android.datafromzappos.databinding.ActivityMain2Binding;
@@ -18,14 +21,13 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class Main2Activity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
         // Get a support ActionBar corresponding to this toolbar
-        ActionBar ab = getSupportActionBar();
 
+        ActionBar ab = getSupportActionBar();
         // Enable the Up button
         ab.setDisplayHomeAsUpEnabled(true);
 
@@ -54,13 +56,21 @@ public class Main2Activity extends AppCompatActivity {
         int itemThatWasClickedId = item.getItemId();
         if (itemThatWasClickedId == R.id.action_favorite) {
             item.setIcon(R.drawable.ic_favorite_black_24dp);
+            Snackbar.make(findViewById(R.id.myCoordinatorLayout), "Set this item as favorite", Snackbar.LENGTH_LONG).show();
             return true;
         }
         if (itemThatWasClickedId == R.id.action_shopping_cart) {
             item.setIcon(R.drawable.ic_shopping_cart_black_24dp);
+            Snackbar.make(findViewById(R.id.myCoordinatorLayout), "Item is added to shopping cart", Snackbar.LENGTH_LONG).show();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void fabShoppingCart(View view) {
+        Snackbar.make(view, "Item is added to shopping cart", Snackbar.LENGTH_LONG).show();
+        // MenuItem item = (MenuItem) findViewById(R.id.action_shopping_cart);
+        // item.setIcon(R.drawable.ic_shopping_cart_black_24dp);
     }
 }
